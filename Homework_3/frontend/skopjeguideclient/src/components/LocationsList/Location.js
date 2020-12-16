2f9418b7cbd2ceecfac1b344055b5ba28c80271d
Ivan Jakimovski <ivan.jakim1004@gmail.com>
2020-12-16 18:32:13 +0100

    Added folder structure

---
+++ Homework_3/frontend/skopjeguideclient/src/components/LocationsList/Location.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+class Location extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>List item</h1>
+            </div>
+        );
+    }
+}
+
+
+export default Location;
