8db0e0c366d0020809d140fd93acb2bd68cb1ccc
Ivan Jakimovski <ivan.jakim1004@gmail.com>
2020-12-16 18:17:52 +0100

    Added hotel controller

---
+++ Homework_3/backend/skopjeguide/src/main/java/mk/ukim/finki/skopjeguide/web/controller/HotelController.java
@@ -0,0 +1,16 @@
+package mk.ukim.finki.skopjeguide.web.controller;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api/hotel")
+public class HotelController {
+    @GetMapping
+    public ResponseEntity getAllHotels() {
+        return ResponseEntity.ok("All Hotels");
+    }
+}
---
+++ Homework_3/frontend/skopjeguideclient/src/components/LocationsList/LocationsList.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+
+class LocationsList extends Component {
+    constructor(props) {
+        super(props);
+
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Locations list</h1>
+            </div>
+        );
+    }
+}
+
+
+export default LocationsList;
