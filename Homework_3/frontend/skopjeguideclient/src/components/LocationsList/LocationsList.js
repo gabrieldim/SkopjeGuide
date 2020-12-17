954d122a86e33cb1b38febbcdbc54bbaf3db0483
ivanovskiviktor <ivanovskiviktor3001@gmail.com>
2020-12-17 18:26:36 +0100

    Added Locations component

--- Homework_3/frontend/skopjeguideclient/src/components/LocationsList/LocationsList.js
+++ Homework_3/frontend/skopjeguideclient/src/components/LocationsList/LocationsList.js
@@ -6,12 +6,26 @@ class LocationsList extends Component {
 
     }
 
+    async componentDidMount() {
+        const data = await fetch('https://skopje-guide.herokuapp.com/api/restaurants');
+        const json = await data.json();
+
+        let list = document.getElementById("locationsList");
+
+        for(let i=0; i<json.length; i++) {
+            list.innerHTML += `<li>${json[i].lon} ${json[i].lat}  ${json[i].name} ${json[i].website}  ${json[i].phone}</li>`;
+        }
+    }
+
     render() {
         return (
             <div>
-                <h1>Locations list</h1>
+            <h1>Locations list</h1>
+        <ul id="locationsList">
+
+            </ul>
             </div>
-        );
+    );
     }
 }
 
