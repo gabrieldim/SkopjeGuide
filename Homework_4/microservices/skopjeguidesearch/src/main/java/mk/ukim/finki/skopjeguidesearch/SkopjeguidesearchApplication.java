c84a844541fdcebb82c808335b926270e00f858f
Ivan Jakimovski <ivan.jakim1004@gmail.com>
2021-01-26 20:00:42 +0100

    Added search microservice

---
+++ Homework_4/microservices/skopjeguidesearch/src/main/java/mk/ukim/finki/skopjeguidesearch/SkopjeguidesearchApplication.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.skopjeguidesearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkopjeguidesearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkopjeguidesearchApplication.class, args);
+	}
+
+}
---
+++ Homework_4/microservices/skopjeguidesearch/src/test/java/mk/ukim/finki/skopjeguidesearch/SkopjeguidesearchApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.skopjeguidesearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkopjeguidesearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
